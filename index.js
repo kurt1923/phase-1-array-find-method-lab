c4acccdb2748b54af6a027b51b48986673de3f8c
Kurt Vermillion <kurtv0727@gmail.com>
2022-07-05 15:06:46 -0400

    commit tree 72c064ce24c8ecf29230c42a8b9558f0420d3300

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function superbowlWin(record){
+    let foundWin = record.find(isWin) // record.find(element => element.result === "W")
+    if (foundWin === undefined){
+    return undefined}
+    else {return foundWin.year}
+}
+function isWin(element){
+    return element.result === "W"
+}
+
+record.find(element => element.result === "W")
